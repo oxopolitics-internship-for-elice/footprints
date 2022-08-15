15077b334527fb7fad73890773d77e3562d50758
Hayeong Kim <fibonacci2246@gmail.com>
2022-08-16 02:14:53 +0900

    chore: 초기 세팅

---
+++ client/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: [
+      { find: '@src', replacement: resolve(__dirname, 'src') },
+      {
+        find: '@components',
+        replacement: resolve(__dirname, 'src/components'),
+      },
+    ],
+  },
+
+  plugins: [react(), tsconfigPaths()],
+});
