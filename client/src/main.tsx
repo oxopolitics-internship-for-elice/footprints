9eb666dec981761f4abe69a82ace0efa88a12968
김하영 <fibonacci2246@gmail.com>
2022-08-19 01:10:24 +0900

    [fix] browserRouter 추가, main.tsx에서 index.css삭제, props 타입파일 삭제

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import Layout from '@components/Layout/Layout';
-import './index.css';
 import GlobalStyle from './styles/Global';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -1,13 +1,13 @@
 import Home from '@/pages/Home';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const Router = () => {
   return (
-    <>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 };
 
