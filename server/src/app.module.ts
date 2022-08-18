f39f0024101dfeab24c8f175f260c76a710c7578
[4기-2반] 장소영 <soy5360@naver.com>
2022-08-18 13:10:32 +0900

    [setup] MongoDB 연결

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import {  Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {MongooseModule} from "@nestjs/mongoose";
+import {ConfigModule} from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI,{
+      useNewUrlParser : true,
+      useUnifiedTopology : true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
