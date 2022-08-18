1aba194bb3709736ebb9d85f2a3ec22f21a2ce8a
장소영 <soy5360@naver.com>
2022-08-19 03:32:15 +0900

    [feat] Issue Dto 작성

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { Issue } from './schemas/IssueSchmea';
 
 @Module({
   imports: [
