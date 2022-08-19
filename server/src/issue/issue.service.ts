a506ac3d0083514c9d6bb5a2df8f2b561753777e
배장한 <bjh4135@gmail.com>
2022-08-19 13:09:28 +0900

    temp

--- server/src/issue/issue.service.ts
+++ server/src/issue/issue.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Issue, IssueDocument } from '../schemas/IssueSchema';
+import { AddIssueDto } from './dto/issue.addIssue.dto';
 
 @Injectable()
-export class IssueService {}
+export class IssueService {
+  constructor(
+    @InjectModel('issues')
+    private readonly issueModel: Model<IssueDocument>,
+  ) {}
+
+  async addIssue(addIssueDto: AddIssueDto): Promise<Issue> {
+
+  }
+}
