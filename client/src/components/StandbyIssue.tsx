1ada8dd1490f1797e4739d1842cf07bb465807aa
황채림 <cofla159@naver.com>
2022-08-20 16:14:41 +0900

    [feature] 찬반 버튼 구현

--- client/src/components/StandbyIssue.tsx
+++ client/src/components/StandbyIssue.tsx
@@ -2,6 +2,8 @@ import { useEffect } from 'react';
 import * as api from '@src/api/api';
 import { standbyIssueState } from '@src/store/StandbyIssueState';
 import { useRecoilState } from 'recoil';
+import styled from '@emotion/styled';
+import { IssueTypes } from '@src/types/IssueTypes';
 
 const StandbyIssue = (): JSX.Element => {
   const [issueList, setIssueList] = useRecoilState(standbyIssueState);
@@ -17,11 +19,29 @@ const StandbyIssue = (): JSX.Element => {
     getIssueList();
   }, []);
 
+  const regiHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const targetElem = e.target as HTMLButtonElement;
+    setIssueList((prev): IssueTypes[] => {
+      const prevIssueList: IssueTypes[] = JSON.parse(JSON.stringify(prev));
+      prevIssueList.forEach((issue) => {
+        if (issue._id === targetElem.dataset.id) {
+          if (targetElem.innerText === '반대') {
+            issue.regi.con = 1 + Number(issue.regi.con);
+          } else {
+            issue.regi.pro = 1 + Number(issue.regi.pro);
+          }
+        }
+      });
+      return prevIssueList;
+    });
+    //db에도 업데이트 해야 함
+  };
+
   if (issueList) {
     return (
       <div>
         {issueList.map((issue) => {
-          const { _id, createdAt, content } = issue;
+          const { _id, createdAt, content, regi } = issue;
           const createdDate = new Date(createdAt);
           const year = createdDate.getFullYear();
           const month = createdDate.getMonth() + 1;
@@ -30,12 +50,17 @@ const StandbyIssue = (): JSX.Element => {
             date >= 10 ? date : '0' + date
           }일`;
           return (
-            <article key={_id}>
+            <Issue key={_id}>
+              <div>{regi.pro - regi.con}/100</div>
               <div>{regiDate}</div>
               <div>{content}</div>
-              <button>찬성</button>
-              <button>반대</button>
-            </article>
+              <button data-id={_id} onClick={regiHandler}>
+                찬성
+              </button>
+              <button data-id={_id} onClick={regiHandler}>
+                반대
+              </button>
+            </Issue>
           );
         })}
       </div>
@@ -46,3 +71,9 @@ const StandbyIssue = (): JSX.Element => {
 };
 
 export default StandbyIssue;
+
+const Issue = styled.article`
+  border: grey 1px solid;
+  border-radius: 2px;
+  padding: 20px;
+`;
