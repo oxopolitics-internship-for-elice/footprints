2ee3736cf1d9998f5b426d15db0d2ad36456d1d6
장소영 <soy5360@naver.com>
2022-08-20 20:03:50 +0900

    [feat] Politician Schema 생성

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { issueSchema } from './schemas/issue.schema';
 import { IssueController } from './issue/issue.controller';
 import { IssueModule } from './issue/issue.module';
 
---
+++ server/src/schemas/politician.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+
+export type PoliticianDocument = Politician & Document;
+
+@Schema({ timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' } })
+export class Politician {
+  @Prop()
+  name: string;
+
+  @Prop()
+  image: string;
+
+  @Prop()
+  party: string;
+
+  @Prop()
+  issues: number[];
+}
+
+export const politicianSchema = SchemaFactory.createForClass(Politician);
