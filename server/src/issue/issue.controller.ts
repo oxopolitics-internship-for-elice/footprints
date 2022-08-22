710a83a4cae564b13f5c06187737d2d948a314a7
장소영 <soy5360@gmail.com>
2022-08-23 00:12:43 +0900

    [fix] object 타입 에러 해결

--- server/src/issue/issue.controller.ts
+++ server/src/issue/issue.controller.ts
@@ -24,114 +24,112 @@ import { IssueService } from './issue.service';
 export class IssueController {
   constructor(private issueService: IssueService) {}
 
-  // 이슈 등록
-  @Post()
-  async addIssue(@Body() issueData: CreateIssueDto, @Res() response) {
-    try {
-      const issue = await this.issueService.addIssue(issueData);
-      return response.status(HttpStatus.OK).json({
-        message: 'create successfully',
-        issue,
-      });
-    } catch (err) {}
-  }
+  // // 이슈 등록
+  // @Post()
+  // async addIssue(@Body() issueData: CreateIssueDto, @Res() response) {
+  //   try {
+  //     const issue = await this.issueService.addIssue(issueData);
+  //     return response.status(HttpStatus.OK).json({
+  //       message: 'create successfully',
+  //       issue,
+  //     });
+  //   } catch (err) {}
+  // }
 
   // 모든 이슈 가져오기(인생 전체 그래프) -> 정치인 api로 옮기면 좋을 듯
   @Get()
-  async getAllIssues(@Res() response) {
+  async getAllIssues() {
     try {
-      const issues = await this.issueService.getAllIssues();
+      const issues = this.issueService.getAllIssues();
       return response.status(HttpStatus.OK).json({ issues });
     } catch (err) {}
   }
 
-  // 정치인 별 이슈 가져오기(10개 사건 그래프)
-  @Get(':politicianId')
-  async getIssuesByPolitician(
-    @Param('politicianId') politicianId: string,
-    @Query() pageOptionsDto: PageOptionsDto,
-    @Res() response,
-  ) {
-    try {
-      const issues = await this.issueService.getIssuesByPolitician(
-        politicianId,
-        pageOptionsDto,
-      );
-      return response.status(HttpStatus.OK).json({ issues });
-    } catch (err) {}
-  }
+  // // 정치인 별 이슈 가져오기(10개 사건 그래프)
+  // @Get(':politicianId')
+  // async getIssuesByPolitician(
+  //   @Param('politicianId') politicianId: string,
+  //   @Query() pageOptionsDto: PageOptionsDto,
+  //   @Res() response,
+  // ) {
+  //   try {
+  //     const issues = await this.issueService.getIssuesByPolitician(
+  //       politicianId,
+  //       pageOptionsDto,
+  //     );
+  //     return response.status(HttpStatus.OK).json({ issues });
+  //   } catch (err) {}
+  // }
 
-  // (관리자) 이슈 내용 수정
-  @Patch(':issueId/content')
-  async setIssueContent(
-    @Param('issueId') id: string,
-    @Body() setIssueContentDto: SetIssueContentDto,
-    @Res() response,
-  ) {
-    try {
-      const issue = await this.issueService.setIssueContent(
-        id,
-        setIssueContentDto,
-      );
-      return response.status(HttpStatus.OK).json({
-        message: 'successfully updated',
-        issue,
-      });
-    } catch (err) {}
-  }
+  // // (관리자) 이슈 내용 수정
+  // @Patch(':issueId/content')
+  // async setIssueContent(
+  //   @Param('issueId') id: string,
+  //   @Body() setIssueContentDto: SetIssueContentDto,
+  //   @Res() response,
+  // ) {
+  //   try {
+  //     const issue = await this.issueService.setIssueContent(
+  //       id,
+  //       setIssueContentDto,
+  //     );
+  //     return response.status(HttpStatus.OK).json({
+  //       message: 'successfully updated',
+  //       issue,
+  //     });
+  //   } catch (err) {}
+  // }
 
-  // (관리자) 이슈 상태 수정
-  @Patch(':issueId/status')
-  async setIssueStatus(
-    @Param('issueId') id: string,
-    @Body() setIssueStatusDto: SetIssueStatusDto,
-    @Res() response,
-  ) {
-    try {
-      const issue = await this.issueService.setIssueStatus(
-        id,
-        setIssueStatusDto,
-      );
-      return response.status(HttpStatus.OK).json({
-        message: 'successfully updated',
-        issue,
-      });
-    } catch (err) {}
-  }
+  // // (관리자) 이슈 상태 수정
+  // @Patch(':issueId/status')
+  // async setIssueStatus(
+  //   @Param('issueId') id: string,
+  //   @Body() setIssueStatusDto: SetIssueStatusDto,
+  //   @Res() response,
+  // ) {
+  //   try {
+  //     const issue = await this.issueService.setIssueStatus(
+  //       id,
+  //       setIssueStatusDto,
+  //     );
+  //     return response.status(HttpStatus.OK).json({
+  //       message: 'successfully updated',
+  //       issue,
+  //     });
+  //   } catch (err) {}
+  // }
 
-  // 이슈 등록 투표
-  @Patch(':issueId/regi')
-  async setIssueRegi(
-    @Param('issueId') id: string,
-    @Body() setIssueRegiDto: SetIssueRegiDto,
-  ) {
-    try {
-      const issue = await this.issueService.setIssueRegiDto(
-        id,
-        setIssueRegiDto,
-      );
-      return response.status(HttpStatus.OK).json({
-        message: 'successfully updated',
-        issue,
-      });
-    } catch (err) {}
-  }
-  
-  // 이슈 여론 투표
-  @Patch(':issueId/poll')
-  async setIssuePoll(
-    @Param('issueId') id: string,
-    @Body() setIssuePollDto: SetIssuePollDto,
-    @Res() response,
-  ) {
-    try {
-      const issue = await this.issueService.setIssuePoll(id, setIssuePollDto);
-      return response.status(HttpStatus.OK).json({
-        message: 'successfully updated',
-        issue,
-      });
-    } catch (err) {}
-  }
+  // // 이슈 등록 투표
+  // @Patch(':issueId/regi')
+  // async setIssueRegi(
+  //   @Param('issueId') id: string,
+  //   @Body() setIssueRegiDto: SetIssueRegiDto,
+  // ) {
+  //   try {
+  //     const issue = await this.issueService.setIssueRegiDto(
+  //       id,
+  //       setIssueRegiDto,
+  //     );
+  //     return response.status(HttpStatus.OK).json({
+  //       message: 'successfully updated',
+  //       issue,
+  //     });
+  //   } catch (err) {}
+  // }
 
-  
+  // // 이슈 여론 투표
+  // @Patch(':issueId/poll')
+  // async setIssuePoll(
+  //   @Param('issueId') id: string,
+  //   @Body() setIssuePollDto: SetIssuePollDto,
+  //   @Res() response,
+  // ) {
+  //   try {
+  //     const issue = await this.issueService.setIssuePoll(id, setIssuePollDto);
+  //     return response.status(HttpStatus.OK).json({
+  //       message: 'successfully updated',
+  //       issue,
+  //     });
+  //   } catch (err) {}
+  // }
 }
