49bef10cc337e8ae45722c3fdc9e5346b37e5aeb
vjvl95 <vjvl95@naver.com>
2022-08-24 17:38:12 +0900

    [feat]Cors 수정

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
