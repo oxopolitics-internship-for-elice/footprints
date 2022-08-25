70033807b4a5b07e2c90da32e96fedecb59ebc5e
황채림 <cofla159@naver.com>
2022-08-26 05:44:06 +0900

    [feat] regi 투표 시 db에 업데이트

--- client/src/components/politician/Issue.tsx
+++ client/src/components/politician/Issue.tsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 import { IssueProps } from '@components/politician/StandbyIssue';
 import { IssueTypes } from '@/types/IssueTypes';
 import dateFormatter from '@/utils/DateFormatter';
+import * as Api from '@/api/Api';
 
 const Issue = ({ issue, setIssueList }: IssueProps): JSX.Element => {
   const { _id, issueDate, content, regi } = issue;
@@ -11,12 +12,22 @@ const Issue = ({ issue, setIssueList }: IssueProps): JSX.Element => {
     const targetElem = e.target as HTMLButtonElement;
     setIssueList((prev: any): IssueTypes[] => {
       const prevIssueList: IssueTypes[] = JSON.parse(JSON.stringify(prev));
-      prevIssueList.forEach(issue => {
+      prevIssueList.forEach(async issue => {
         if (issue._id === targetElem.dataset.id) {
           if (targetElem.innerText === '반대') {
             issue.regi.con += 1;
+            const res = await Api.patch(`issues/${_id}/regi`, {
+              pro: false,
+              con: true,
+            });
+            console.log(res);
           } else {
             issue.regi.pro += 1;
+            const res = await Api.patch(`issues/${_id}/regi`, {
+              pro: true,
+              con: false,
+            });
+            console.log(res);
           }
         }
       });
@@ -26,7 +37,8 @@ const Issue = ({ issue, setIssueList }: IssueProps): JSX.Element => {
 
   return (
     <IssueContainer>
-      <div>{regi.pro - regi.con}/100</div>
+      <span>{`찬성 ${regi.pro}`}</span>
+      <span>{`반대 ${regi.con}`}</span>
       <div>{issuedDate}</div>
       <div>{content}</div>
       <button data-id={_id} onClick={regiHandler}>
