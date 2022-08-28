7d29aefd05220cdc7838a58b7cb194d0523736e6
김하영 <fibonacci2246@gmail.com>
2022-08-29 00:10:32 +0900

    [chore] remove all cache

--- client/src/utils/cookie.ts
+++
@@ -1,8 +0,0 @@
-import { Cookies } from 'react-cookie';
-
-const cookies = new Cookies();
-
-export const getCookie = (name: string) => cookies.get(name);
-export const setCookie = (name: string, value: string) =>
-  cookies.set(name, value);
-export const removeCookie = (name: string) => cookies.remove(name);
