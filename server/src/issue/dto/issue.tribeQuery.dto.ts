a4f1a8ccd0feb81cff342129a854e1c6f18a74af
장소영 <soy5360@gmail.com>
2022-08-31 02:59:00 +0900

    [chore] 필요없는 dto 삭제

--- server/src/issue/dto/issue.tribeQuery.dto.ts
+++
@@ -1,8 +0,0 @@
-import { Transform } from 'class-transformer';
-import { IsOptional, IsString } from 'class-validator';
-
-export class QueryTribeDto {
-  @IsString()
-  @IsOptional()
-  readonly tribe: string;
-}
