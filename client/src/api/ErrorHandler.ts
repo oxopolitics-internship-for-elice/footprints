8f0cdda22deed7cb10cb98edcaa8acf3fafe67c3
김하영 <fibonacci2246@gmail.com>
2022-08-31 03:13:17 +0900

    [fix] 에러 핸들러 복구

---
+++ client/src/api/ErrorHandler.ts
@@ -0,0 +1,24 @@
+import { Alert } from '@/components/Base/Alert';
+import axios, { AxiosError } from 'axios';
+
+const errorHandler = (error: unknown) => {
+  if (axios.isAxiosError(error)) {
+    Alert.fire({
+      icon: 'error',
+      title: `error message:${error.message}`,
+    });
+    return error.message;
+  } else {
+    let message;
+    if (error instanceof Error) message = error.message;
+    else message = String(error);
+
+    Alert.fire({
+      icon: 'error',
+      title: `error message:${message}`,
+    });
+    return message;
+  }
+};
+
+export default errorHandler;
