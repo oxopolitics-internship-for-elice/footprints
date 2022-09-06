fb09567cd6c7f5de06e3a007da9453ef9263d883
배장한 <bjh4135@gmail.com>
2022-09-06 14:10:39 +0900

    [feat] testing code 작성 in politician controller

--- server/src/politician/politician.controller.spec.ts
+++ server/src/politician/politician.controller.spec.ts
@@ -1,15 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from '../user/user.service';
 import { PoliticianController } from './politician.controller';
+import { PoliticianService } from './politician.service';
 
 describe('PoliticianController', () => {
   let controller: PoliticianController;
+  let service: PoliticianService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PoliticianController],
+      providers: [PoliticianService, UserService],
     }).compile();
 
     controller = module.get<PoliticianController>(PoliticianController);
+    service = module.get<PoliticianService>(PoliticianService);
+  });
+
+  describe('getAllPoliticians', () => {
+    it('should return an array of politicians', async () => {
+      const result = ['test'];
+      jest.spyOn(service, 'getAllPoliticians').mockImplementation(async () => result);
+
+      expect(await controller.getAllPoliticians(result)).toBe(result);
+    });
   });
 
   it('should be defined', () => {
