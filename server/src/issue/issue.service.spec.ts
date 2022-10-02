30f811fa8eb00328875db32c62fd0d40e0286d90
Janghan Bae <bjh4135@gmail.com>
2022-10-02 15:42:19 +0900

    [chore] test code 세팅

--- server/src/issue/issue.service.spec.ts
+++ server/src/issue/issue.service.spec.ts
@@ -1,12 +1,25 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { connect, Connection, Model } from 'mongoose';
+import { Issue, issueSchema } from 'src/schemas/issue.schema';
 import { IssueService } from './issue.service';
 
 describe('IssueService', () => {
+  let mongoConnection: Connection;
   let service: IssueService;
+  let issueModel: Model<Issue>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    mongoConnection = (await connect('mongodb://127.0.0.1:27017')).connection;
+    issueModel = mongoConnection.model(Issue.name, issueSchema);
     const module: TestingModule = await Test.createTestingModule({
-      providers: [IssueService],
+      providers: [
+        IssueService,
+        {
+          provide: getModelToken(Issue.name),
+          useValue: issueModel,
+        },
+      ],
     }).compile();
 
     service = module.get<IssueService>(IssueService);
@@ -14,5 +27,9 @@ describe('IssueService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  afterAll(async () => {
+    await mongoConnection.close();
   });
 });
