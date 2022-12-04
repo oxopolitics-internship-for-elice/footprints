b456fa314f9338d3df8f16a18e0c0f57cbd9db3f
김하영 <fibonacci2246@gmail.com>
2022-12-04 17:41:47 +0900

    modify: kakao 로그인 시 서버측에서 res.cookie로 저장

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -19,8 +19,15 @@ export class AuthController {
     // console.log('req.user: ', req.user);
     // console.log('accessToken', req.user.accessToken);
     // console.log('refresh_token', req.user.refreshToken);
+    if (req.user.type === 'login') {
+      res.cookie('access_token', req.user.accessToken);
+      res.cookie('refresh_token', req.user.refreshToken);
+    } else {
+      res.cookie('once_token', req.user.onceToken);
+    }
 
-    res.redirect(`${process.env.CLIENT_HOST}?access=${req.user.accessToken}&refresh=${req.user.refreshToken}`);
+    // res.redirect(`${process.env.CLIENT_HOST}?access=${req.user.accessToken}&refresh=${req.user.refreshToken}`);
+    res.redirect(`${process.env.CLIENT_HOST}`);
     res.end();
   }
 }
