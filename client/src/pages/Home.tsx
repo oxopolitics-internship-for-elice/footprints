547a21fd93dae66cb80ba88d8aef8a7d7298f293
김하영 <fibonacci2246@gmail.com>
2022-12-07 10:57:31 +0900

    modify: URL 스트링쿼리 토큰 세팅 로직 제거
    access_token, refersh_token 서버에서 설정하도록 바꿨으므로 home의 useEffect로직에 들어가던 URL에 따라서 토큰을 세팅해주는 로직을 삭제하였습니다.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,36 +2,8 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import Header from '@/components/base/Header';
 import SlideFullPage from '@/components/home/SlideFullPage';
-import { setCookie } from '@/utils/Cookie';
-import { useSetRecoilState } from 'recoil';
-import { authTokenState } from '@/store/AuthTokenState';
 
 const Home = () => {
-  const urlSearchParams = new URLSearchParams(window.location.search);
-  const params = Object.fromEntries(urlSearchParams.entries());
-  const setAuthTokenState = useSetRecoilState(authTokenState);
-
-  const handleLogin = () => {
-    const { access, refresh } = params;
-    if (access && refresh) {
-      setCookie('access_token', access, {
-        path: '/',
-        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 5),
-        httpOnly: true,
-      });
-      setCookie('refresh_token', refresh, {
-        path: '/',
-        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 5),
-        httpOnly: true,
-      });
-      setAuthTokenState(prev => (prev = { access_token: access }));
-    }
-  };
-
-  React.useEffect(() => {
-    handleLogin();
-  }, []);
-
   return (
     <>
       <Helmet>
